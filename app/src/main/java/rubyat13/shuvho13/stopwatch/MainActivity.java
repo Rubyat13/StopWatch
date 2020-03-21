61b3470eafecfb98af33c7b67e9ec4108d1e3926
rubyat13 <alrubyatshuvho@gmail.com>
2020-03-21 21:33:25 +0600

    2nd commit

--- app/src/main/java/rubyat13/shuvho13/stopwatch/MainActivity.java
+++ app/src/main/java/rubyat13/shuvho13/stopwatch/MainActivity.java
@@ -2,13 +2,35 @@ package rubyat13.shuvho13.stopwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.graphics.Typeface.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvSplash, tvSubSplash;
+    Button bSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvSplash = findViewById(R.id.tvSplash);
+        tvSubSplash = findViewById(R.id.tvSubSplash);
+        bSplash = findViewById(R.id.bSplash);
+
+        //Import font
+        Typeface MLight = Typeface.createFromAsset(getAssets(),  "fonts/MLight.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(),  "fonts/MMedium.ttf");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(),  "fonts/MRegular.ttf");
+
+        // Customize font
+        tvSplash.setTypeface(MRegular);
+        tvSubSplash.setTypeface(MLight);
+        bSplash.setTypeface(MMedium);
     }
 }
